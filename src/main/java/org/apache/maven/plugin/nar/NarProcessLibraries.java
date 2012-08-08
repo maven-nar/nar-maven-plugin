5f9a700078e4e7d357dbcb4454f739c96a7ad743
ericker <richard.kerr@ericsson.com>
2012-08-08 16:13:45 +0100

    Fixed mapping of out dir and filename for different output types (static, shared, executable, etc)

--- src/main/java/org/apache/maven/plugin/nar/NarProcessLibraries.java
+++ src/main/java/org/apache/maven/plugin/nar/NarProcessLibraries.java
@@ -7,10 +7,11 @@ import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 
 /**
- * Adds the ability to run command line tools to post-process the compiled
- * libraries (ie: ranlib/ar/etc)
+ * Adds the ability to run arbitrary command line tools to post-process the
+ * compiled output (ie: ranlib/ar/etc)
  * 
  * @goal nar-process-libraries
  * @phase process-classes
@@ -27,36 +28,41 @@ public class NarProcessLibraries extends AbstractCompileMojo {
      */
     private List<ProcessLibraryCommand> commands;
 
+    private Log log = getLog();
+
     @Override
     public void narExecute() throws MojoFailureException, MojoExecutionException {
-	getLog().info("Running process libraries");
+	log.info("Running process libraries");
 	// For each of the libraries defined for this build
 	for (Library library : getLibraries()) {
-	    getLog().info("Processing library " + library);
+	    log.info("Processing library " + library);
 	    String type = library.getType();
-	    File outDir;
+	    File outFile;
 	    // Find what the output directory is
 	    if (type.equalsIgnoreCase(Library.EXECUTABLE)) {
-		outDir = getLayout().getBinDirectory(getTargetDirectory(), getMavenProject().getArtifactId(),
+		File outDir = getLayout().getBinDirectory(getTargetDirectory(), getMavenProject().getArtifactId(),
 			getMavenProject().getVersion(), getAOL().toString());
+		outFile = new File(outDir, getOutput(false));
 	    } else {
-		outDir = getLayout().getLibDirectory(getTargetDirectory(), getMavenProject().getArtifactId(),
+		File outDir = getLayout().getLibDirectory(getTargetDirectory(), getMavenProject().getArtifactId(),
 			getMavenProject().getVersion(), getAOL().toString(), type);
+		outFile = new File(outDir, getOutput(true));
 	    }
 	    // Then run the commands that are applicable for this library type
 	    for (ProcessLibraryCommand command : commands) {
 		if (command.getType().equalsIgnoreCase(type))
-		    runCommand(command, outDir);
+		    runCommand(command, outFile);
 	    }
 	}
 
     }
 
-    private void runCommand(ProcessLibraryCommand command, File outputDirectory) throws MojoFailureException, MojoExecutionException {
+    private void runCommand(ProcessLibraryCommand command, File outputFile) throws MojoFailureException,
+	    MojoExecutionException {
 	ProcessBuilder p = new ProcessBuilder(command.getCommandList());
-	p.command().add(outputDirectory.toString() + "/" + getOutput(false));
+	p.command().add(outputFile.toString());
 	p.redirectErrorStream(true);
-	getLog().info("Running command \"" + p.command() + "\"");
+	log.info("Running command \"" + p.command() + "\"");
 	try {
 	    Process process = p.start();
 	    BufferedInputStream bis = new BufferedInputStream(process.getInputStream());
@@ -64,19 +70,20 @@ public class NarProcessLibraries extends AbstractCompileMojo {
 	    int endOfStream = 0;
 	    do {
 		endOfStream = bis.read(buffer);
-		getLog().debug(new String(buffer, 0, endOfStream == -1 ? 0 : endOfStream));
+		log.debug(new String(buffer, 0, endOfStream == -1 ? 0 : endOfStream));
 	    } while (endOfStream != -1);
-	    
+
 	    if (process.waitFor() != 0) {
-		// TODO: Maybe this shouldn't be an exception, it might have still worked?!
+		// TODO: Maybe this shouldn't be an exception, it might have
+		// still worked?!
 		throw new MojoFailureException("Process exited abnormally");
 	    }
 	} catch (IOException e) {
 	    e.printStackTrace();
-	    throw new MojoFailureException("Failed to run the command \"" + p.command() + "\"");
+	    throw new MojoFailureException("Failed to run the command \"" + p.command() + "\"", e);
 	} catch (InterruptedException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
+	    throw new MojoFailureException("Failed to run the command \"" + p.command() + "\"", e);
 	}
     }
 
