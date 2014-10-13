6148bfadb439d4bc302bfbf3f1d88147917343fa
Rui Maranhao <rui.maranhao@gmail.com>
2014-10-13 13:05:21 -0700

    allow compilation of files with same names

--- src/main/java/com/github/maven_nar/cpptasks/compiler/CommandLineCompilerConfiguration.java
+++ src/main/java/com/github/maven_nar/cpptasks/compiler/CommandLineCompilerConfiguration.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ import com.github.maven_nar.cpptasks.ProcessorParam;
 import com.github.maven_nar.cpptasks.VersionInfo;
 /**
  * A configuration for a C++ compiler
- * 
+ *
  * @author Curt Arnold
  */
 public final class CommandLineCompilerConfiguration
@@ -158,23 +158,23 @@ public final class CommandLineCompilerConfiguration
         }
     }
     /**
-     * 
+     *
      * This method may be used to get two distinct compiler configurations, one
      * for compiling the specified file and producing a precompiled header
      * file, and a second for compiling other files using the precompiled
      * header file.
-     * 
+     *
      * The last (preferrably only) include directive in the prototype file will
      * be used to mark the boundary between pre-compiled and normally compiled
      * headers.
-     * 
+     *
      * @param prototype
      *            A source file (for example, stdafx.cpp) that is used to build
      *            the precompiled header file. @returns null if precompiled
      *            headers are not supported or a two element array containing
      *            the precompiled header generation configuration and the
      *            consuming configuration
-     *  
+     *
      */
     public CompilerConfiguration[] createPrecompileConfigurations(
             File prototype, String[] nonPrecompiledFiles) {
@@ -243,5 +243,8 @@ public final class CommandLineCompilerConfiguration
     }
     public final String getCommandPath() {
         return this.commandPath;
+    }
+    public String getOutputSuffix() {
+        return compiler.getOutputSuffix();
     }
 }
