602529fd01f31c5159a479cb66bab86eae929607
Greg Domjan <gorf4673@gmail.com>
2015-08-12 13:34:01 -0600

    MSVC WindowsSDK single digit major version - WindowsSDK 10 support

--- src/main/java/com/github/maven_nar/Msvc.java
+++ src/main/java/com/github/maven_nar/Msvc.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Comparator;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -39,6 +40,7 @@ public class Msvc {
 
   private File windowsHome;
   private String toolPathWindowsSDK;
+  private String versionPathWindowsSDK = "";
   private String toolPathLinker;
 
   private boolean addIncludePath(final CCTask task, final File home, final String subDirectory)
@@ -76,10 +78,6 @@ public class Msvc {
     return false;
   }
 
-  public int compareVersion(final String version1, final String version2) {
-    return version1.replace(".", "").compareTo(version2.replace(".", ""));
-  }
-
   public void configureCCTask(final CCTask task) throws MojoExecutionException {
     final String os = mojo.getOS();
     if (os.equals(OS.WINDOWS)) {
@@ -88,8 +86,10 @@ public class Msvc {
       if (compareVersion(this.windowsSdkVersion, "7.1A") <= 0) {
         addIncludePath(task, this.windowsSdkHome, "include");
       } else {
-        addIncludePath(task, this.windowsSdkHome, "include/shared");
-        addIncludePath(task, this.windowsSdkHome, "include/um");
+        addIncludePath(task, this.windowsSdkHome, "include" + versionPathWindowsSDK + "/ucrt");
+        addIncludePath(task, this.windowsSdkHome, "include" + versionPathWindowsSDK + "/um");
+        addIncludePath(task, this.windowsSdkHome, "include" + versionPathWindowsSDK + "/shared");
+        addIncludePath(task, this.windowsSdkHome, "include" + versionPathWindowsSDK + "/winrt");
       }
       task.addEnv(getPathVariable());
       // TODO: supporting running with clean environment - addEnv sets
@@ -131,17 +131,24 @@ public class Msvc {
         sdkArch = "x64";
       }
       // 6 lib ?+ lib/x86 or lib/x64
-      if (compareVersion(this.windowsSdkVersion, "8.0") < 0) { 
+      if (compareVersion(this.windowsSdkVersion, "8.0") < 0) {
         if ("x86".equals(arch)) {
           linker.addLibraryDirectory(this.windowsSdkHome, "lib");
         } else {
           linker.addLibraryDirectory(this.windowsSdkHome, "lib/" + sdkArch);
         }
       } else if (compareVersion(this.windowsSdkVersion, "8.0") == 0) {
-        linker.addLibraryDirectory(this.windowsSdkHome, "Lib/Win8/um/" + sdkArch);
+        linker.addLibraryDirectory(this.windowsSdkHome, "Lib/Win8/um/" + sdkArch); // 8+
       } else {
-        linker.addLibraryDirectory(this.windowsSdkHome, "Lib/Winv6.3/um/" + sdkArch);
+        linker.addLibraryDirectory(this.windowsSdkHome, "Lib/Winv6.3/um/" + sdkArch); // win
+                                                                                      // 8
+                                                                                      // sdk
+                                                                                      // compatible
+                                                                                      // with
+                                                                                      // XP
       }
+      linker.addLibraryDirectory(this.windowsSdkHome, "Lib" + versionPathWindowsSDK + "/ucrt/" + sdkArch); // 10+
+      linker.addLibraryDirectory(this.windowsSdkHome, "Lib" + versionPathWindowsSDK + "/um/" + sdkArch); // 10+
     }
   }
 
@@ -187,7 +194,8 @@ public class Msvc {
     // 32bit - force 32 on 64bit mojo(x86 / x86_amd64)
     // match mojo - os x86 is as above; os x64 mojo (x86 / amd64)
 
-    // Cross tools first if necessary, platform tools second, more generic tools later
+    // Cross tools first if necessary, platform tools second, more generic tools
+    // later
     if (!osArchitecture.equals(mojoArchitecture) && !matchMojo) {
       if (!addPath(this.home, "VC/bin/" + osArchitecture + "_" + mojoArchitecture)) {
         throw new MojoExecutionException("Unable to find compiler for architecture " + mojoArchitecture + ".\n"
@@ -261,8 +269,8 @@ public class Msvc {
         // able to acccess - HKLM\SOFTWARE\Microsoft\Visual
         // Studio\Major.Minor:InstallDir
       }
-      mojo.getLog().debug(
-          String.format(" VisualStudio %1s (%2s) found %3s ", this.version, internalVersion, this.home));
+      mojo.getLog()
+          .debug(String.format(" VisualStudio %1s (%2s) found %3s ", this.version, internalVersion, this.home));
     } else {
       this.version = "";
       File home = null;
@@ -279,7 +287,8 @@ public class Msvc {
               this.version = version;
               this.home = commonToolsDirectory.getParentFile().getParentFile();
               mojo.getLog().debug(
-                  String.format(" VisualStudio %1s (%2s) found %3s ", this.version, matcher.group(1) + matcher.group(2), this.home));
+                  String.format(" VisualStudio %1s (%2s) found %3s ", this.version,
+                      matcher.group(1) + matcher.group(2), this.home));
             }
           }
         }
@@ -297,7 +306,8 @@ public class Msvc {
         if (m.find()) {
           this.version = m.group(1);
           mojo.getLog().debug(
-              String.format(" VisualStudio Not found but link runs and reports version %1s (%2s)", this.version, m.group(0)));
+              String.format(" VisualStudio Not found but link runs and reports version %1s (%2s)", this.version,
+                  m.group(0)));
         } else {
           throw new MojoExecutionException(
               "msvc.version not specified and no VS<Version>COMNTOOLS environment variable can be found");
@@ -325,19 +335,50 @@ public class Msvc {
               if (kitVersion.charAt(0) == 'v') {
                 kitVersion = kitVersion.substring(1);
               }
-              mojo.getLog().debug(String.format(" WindowSDK %1s found %2s",kitVersion,kitDirectory.getAbsolutePath()));
-              if (kitVersion.matches("\\d+\\.\\d+[A-Z]?")) {
+              mojo.getLog()
+                  .debug(String.format(" WindowSDK %1s found %2s", kitVersion, kitDirectory.getAbsolutePath()));
+              if (kitVersion.matches("\\d+\\.\\d+?[A-Z]?")) { // windows 8/8.1
+                                                              // SDK
                 if (this.windowsSdkVersion.length() == 0) {
                   if (compareVersion(kitVersion, maxVersion) > 0) {
                     maxVersion = kitVersion;
                     home = kitDirectory;
                   }
                 } else {
-                  // TODO: is it acceptable to be sloppy - msvc installs 'A' version, windows platform sdk installs more complete
-                  // if (this.windowsSdkVersion.startsWith(kitVersion)) {  
+                  // TODO: is it acceptable to be sloppy - msvc installs 'A'
+                  // version, windows platform sdk installs more complete
+                  // if (this.windowsSdkVersion.startsWith(kitVersion)) {
                   if (kitVersion.equals(this.windowsSdkVersion)) {
                     maxVersion = this.windowsSdkVersion;
                     home = kitDirectory;
+                  }
+                }
+              }
+              if (kitVersion.matches("\\d+?")) { // windows 10 SDK supports
+                                                 // multiple installs
+                home = kitDirectory;
+                final File[] kitVersionDirectories = new File(kitDirectory, "Include").listFiles();
+                for (final File kitVersionDirectory : kitVersionDirectories) {
+                  mojo.getLog().debug(String.format(" KitDir %1s", kitVersionDirectory.getAbsolutePath()));
+                  if (new File(kitVersionDirectory, "um").exists()) {
+                    String kitSpecificVersion = kitVersionDirectory.getName();
+                    if (this.windowsSdkVersion.length() == 0 || kitVersion.equals(this.windowsSdkVersion)) { // unspecified
+                                                                                                             // or
+                                                                                                             // generic
+                                                                                                             // 10
+                      // find the max version available.
+                      mojo.getLog().debug(String.format(" KitVer %1s, maxVer %2s", kitSpecificVersion, maxVersion));
+                      if (compareVersion(kitSpecificVersion, maxVersion) > 0) {
+                        maxVersion = kitSpecificVersion;
+                        versionPathWindowsSDK = "/" + maxVersion;
+                      }
+                    } else {
+                      // find the specific version
+                      if (kitVersion.equals(this.windowsSdkVersion)) {
+                        maxVersion = kitSpecificVersion;
+                        versionPathWindowsSDK = "/" + maxVersion;
+                      }
+                    }
                   }
                 }
               }
@@ -350,10 +391,10 @@ public class Msvc {
       this.windowsSdkHome = home;
     }
     if (maxVersion.length() == 0) {
-      throw new MojoExecutionException("msvc.windowsSdkVersion not specified and versions can be found");
+      throw new MojoExecutionException("msvc.windowsSdkVersion not specified and versions cannot be found");
     }
     this.windowsSdkVersion = maxVersion;
-    mojo.getLog().debug(String.format(" Using WindowSDK %1s found %2s",this.windowsSdkVersion,this.windowsSdkHome));
+    mojo.getLog().debug(String.format(" Using WindowSDK %1s found %2s", this.windowsSdkVersion, this.windowsSdkHome));
   }
 
   public void setMojo(final AbstractNarMojo mojo) throws MojoFailureException, MojoExecutionException {
@@ -368,13 +409,157 @@ public class Msvc {
     return this.home + "\n" + this.windowsSdkHome;
   }
 
-  public String getToolPath() {return this.toolPathLinker;}
-  public String getSDKToolPath() {return this.toolPathWindowsSDK;}
+  public String getToolPath() {
+    return this.toolPathLinker;
+  }
+
+  public String getSDKToolPath() {
+    return this.toolPathWindowsSDK;
+  }
+
   public void setToolPath(CompilerDef compilerDef, String name) {
     if ("res".equals(name) || "mc".equals(name) || "idl".equals(name)) {
       compilerDef.setToolPath(this.toolPathWindowsSDK);
     } else {
       compilerDef.setToolPath(this.toolPathLinker);
+    }
+  }
+
+  public int compareVersion(Object o1, Object o2) {
+    String version1 = (String) o1;
+    String version2 = (String) o2;
+
+    VersionTokenizer tokenizer1 = new VersionTokenizer(version1);
+    VersionTokenizer tokenizer2 = new VersionTokenizer(version2);
+
+    int number1 = 0, number2 = 0;
+    String suffix1 = "", suffix2 = "";
+
+    while (tokenizer1.MoveNext()) {
+      if (!tokenizer2.MoveNext()) {
+        do {
+          number1 = tokenizer1.getNumber();
+          suffix1 = tokenizer1.getSuffix();
+          if (number1 != 0 || suffix1.length() != 0) {
+            // Version one is longer than number two, and non-zero
+            return 1;
+          }
+        } while (tokenizer1.MoveNext());
+
+        // Version one is longer than version two, but zero
+        return 0;
+      }
+
+      number1 = tokenizer1.getNumber();
+      suffix1 = tokenizer1.getSuffix();
+      number2 = tokenizer2.getNumber();
+      suffix2 = tokenizer2.getSuffix();
+
+      if (number1 < number2) {
+        // Number one is less than number two
+        return -1;
+      }
+      if (number1 > number2) {
+        // Number one is greater than number two
+        return 1;
+      }
+
+      boolean empty1 = suffix1.length() == 0;
+      boolean empty2 = suffix2.length() == 0;
+
+      if (empty1 && empty2)
+        continue; // No suffixes
+      if (empty1)
+        return 1; // First suffix is empty (1.2 > 1.2b)
+      if (empty2)
+        return -1; // Second suffix is empty (1.2a < 1.2)
+
+      // Lexical comparison of suffixes
+      int result = suffix1.compareTo(suffix2);
+      if (result != 0)
+        return result;
+
+    }
+    if (tokenizer2.MoveNext()) {
+      do {
+        number2 = tokenizer2.getNumber();
+        suffix2 = tokenizer2.getSuffix();
+        if (number2 != 0 || suffix2.length() != 0) {
+          // Version one is longer than version two, and non-zero
+          return -1;
+        }
+      } while (tokenizer2.MoveNext());
+
+      // Version two is longer than version one, but zero
+      return 0;
+    }
+    return 0;
+  }
+
+  // VersionTokenizer.java
+  class VersionTokenizer {
+    private final String _versionString;
+    private final int _length;
+
+    private int _position;
+    private int _number;
+    private String _suffix;
+    private boolean _hasValue;
+
+    public int getNumber() {
+      return _number;
+    }
+
+    public String getSuffix() {
+      return _suffix;
+    }
+
+    public boolean hasValue() {
+      return _hasValue;
+    }
+
+    public VersionTokenizer(String versionString) {
+      if (versionString == null)
+        throw new IllegalArgumentException("versionString is null");
+
+      _versionString = versionString;
+      _length = versionString.length();
+    }
+
+    public boolean MoveNext() {
+      _number = 0;
+      _suffix = "";
+      _hasValue = false;
+
+      // No more characters
+      if (_position >= _length)
+        return false;
+
+      _hasValue = true;
+
+      while (_position < _length) {
+        char c = _versionString.charAt(_position);
+        if (c < '0' || c > '9')
+          break;
+        _number = _number * 10 + (c - '0');
+        _position++;
+      }
+
+      int suffixStart = _position;
+
+      while (_position < _length) {
+        char c = _versionString.charAt(_position);
+        if (c == '.')
+          break;
+        _position++;
+      }
+
+      _suffix = _versionString.substring(suffixStart, _position);
+
+      if (_position < _length)
+        _position++;
+
+      return true;
     }
   }
 }
