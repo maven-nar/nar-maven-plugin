c9665a89816487a2e18a2be82e7d83830c9f683c
Curtis Rueden <ctrueden@wisc.edu>
2019-05-23 17:13:01 -0500

    Run the integration tests only once
    It appears that they run during the integration-test phase already.
    So hopefully there is no need to add the run goal explicitly again.
    
    Also, in Travis CI, echo more clearly when/how they are being run.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,6 +3,8 @@ curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/trav
 sh travis-build.sh $encrypted_f76761764219_key $encrypted_f76761764219_iv &&
 if [ ! -f release.properties ]
 then
+  echo
+  echo '== No release.properties; running integration tests =='
   # Not a release -- also perform integration tests.
   mvn -Dinvoker.debug=true -Prun-its
 fi
