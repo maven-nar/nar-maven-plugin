cff1f9a3ceb152fcd767e48f3c5eb1a8419c6694
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-03-02 15:16:59 +0100

    Add bash traces

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_f76761764219_key $encrypted_f76761764219_iv &&
+sh -x travis-build.sh $encrypted_f76761764219_key $encrypted_f76761764219_iv &&
 if [ ! -f release.properties ]
 then
   echo
